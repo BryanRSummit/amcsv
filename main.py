3639026f390adc7876746f9b1dad466594dd89a4
BryanRSummit <bryan@reddsummit.com>
2024-06-18 12:05:28 -0600

    Working, need to ask if people want other information

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from simple_salesforce import Salesforce
+import os
 import json
+import pickle
+import csv
 from sf_query import is_untouched
 
 def sf_login():
@@ -13,5 +16,46 @@ def sf_login():
 
 if __name__ == "__main__":
     sf = sf_login()
-    untouched_by_agent = is_untouched(sf)
-    print("hey")
+
+    # ------------------------------------- Pickle file handling -------------------------
+    # Path to the pickle file
+    pickle_file = 'agents.pickle'
+    # Check if the file exists
+    if os.path.isfile(pickle_file):
+        print(f"The file '{pickle_file}' already exists.")
+        # Load the dictionary from the file
+        with open(pickle_file, 'rb') as file:
+            agent_dict = pickle.load(file)
+    else:
+        print(f"The file '{pickle_file}' does not exist.")
+        # Create your dictionary here
+        untouched_by_agent = is_untouched(sf)
+
+        # Save the dictionary to the file
+        with open(pickle_file, 'wb') as file:
+            pickle.dump(untouched_by_agent, file, protocol=pickle.HIGHEST_PROTOCOL)
+    # ------------------------------------- Pickle file handling -------------------------
+
+
+    column_names = ["Account Name", "Account Link", "Account Type", "Map Created", "Account Managers"]
+    # Loop through each agent in the dictionary
+    for agent, accounts in agent_dict.items():
+        # Create a CSV file for the agent
+        csv_filename = f"{agent}_uncontacted.csv"
+        with open(csv_filename, "w", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=column_names)
+            
+            # Write the column names as the header
+            writer.writeheader()
+            
+            # Write each account as a row in the CSV file
+            for account in accounts:
+                account_dict = {
+                    "Account Name": account.name,
+                    "Account Link": f"https://reddsummit.lightning.force.com/lightning/r/Account/{account.id}/view",
+                    "Account Type": account.account_type,
+                    "Map Created": account.mapCreated,
+                }
+                writer.writerow(account_dict)
+
+    print("CSV files created successfully.")
